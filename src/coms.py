990ec7da3c6f0dad9a929496d7b17bd5a6d6531d
NikolayBlagoev <workerthreadllc@gmail.com>
2026-03-18 18:04:00 +0200

    log

--- src/coms.py
+++ src/coms.py
@@ -25,6 +25,9 @@ def gather(out: torch.Tensor, rank, world_size, group = None):
     _t1 = [torch.zeros(tensor_shapes) for _ in range(world_size)]
     dist.all_gather(_t1,torch.tensor(list(out.shape)), group=group)
     _t2 = [torch.zeros(_t1[id].tolist(), dtype = out.dtype, device = out.device) for id in range(world_size)]
+    for elm in _t2:
+        print(elm.shape,elm.dtype,elm.device)
+    print(out.shape,out.dtype,out.device)
     dist.all_gather(_t2,out, group=group)
     return _t2
 
