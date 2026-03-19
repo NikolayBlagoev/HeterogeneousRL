5514fee329b7116e03e49e1798f409cc6317344c
NikolayBlagoev <workerthreadllc@gmail.com>
2026-03-19 11:29:08 +0200

    reduce mb size

--- src/grpo.py
+++ src/grpo.py
@@ -39,7 +39,7 @@ class GRPOConfig():
     epsilon: float = 0.2
     epsilon_low: float = None
     epsilon_high: float = None
-    micro_batch_size: int = 4
+    micro_batch_size: int = 3
     steps_per_generation: int = 1
     clip_gradient: float = 1.0
     def __post_init__(self):
