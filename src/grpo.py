f25698b05d598083c7fa392dbc18b5562b37d259
NikolayBlagoev <workerthreadllc@gmail.com>
2026-04-20 18:03:12 +0300

    smaller schedule

--- src/grpo.py
+++ src/grpo.py
@@ -207,7 +207,7 @@ def grpo_train_loop(model, optimizer, replay_buffer, grpo_config: GRPOConfig, re
                 tmp_pkl = tmp_pkl.tolist()
             for idx,adv in enumerate(exp.advantages[rng[0]:rng[1]]):
                 adv = adv.item()
-                if adv <= 0 and tmp_pkl[idx] > 2+100*(50-step)/50:
+                if adv <= 0 and tmp_pkl[idx] > 2+25*(50-step)/50:
                     # print("need to drop",idx)
                     drop.append(idx)
             foreign = "f-" in method and len(drop) > 0
